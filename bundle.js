89f20ca9db7756c3f6012760b60759f201b3b433
DanielJDufour <daniel.j.dufour@gmail.com>
2017-11-28 21:17:14 +0000

    added local hosting of example_4326.tif

--- bundle.js
+++ bundle.js
@@ -11,13 +11,22 @@ L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-var url_to_geotiff_file = "https://geotiff.io/data/example_4326.tif";
+var url_to_geotiff_file = "/example_4326.tif";
 
 fetch(url_to_geotiff_file)
   .then(response => response.arrayBuffer())
   .then(arrayBuffer => {
     parse_georaster(arrayBuffer).then(georaster => {
       console.log("georaster:", georaster);
+
+      /*
+          GeoRasterLayer is an extension of GridLayer,
+          which means can use GridLayer options like opacity.
+
+          Just make sure to include the georaster option!
+
+          http://leafletjs.com/reference-1.2.0.html#gridlayer
+      */
       var layer = new GeoRasterLayer({
           georaster: georaster,
           opacity: 0.7
